35cba67ad8c192377703da5edf980146a2d77c14
Hugo van den Berg <hugo.van.den.berg@brabantwater.nl>
2019-08-27 11:01:44 +0200

    fixes PostgreSQL syntax in createpositoranddefault.js

--- anchor_modeler/SQL/PostgreSQL/crt/CreatePositorAndDefault.js
+++ anchor_modeler/SQL/PostgreSQL/crt/CreatePositorAndDefault.js
@@ -5,20 +5,17 @@
 -- must be available the table is set up with a default positor with identity 0.
 --
 -- Positor table ------------------------------------------------------------------------------------------------------
-IF Object_ID('$schema.metadata.encapsulation$._$schema.metadata.positorSuffix', 'U') IS NULL
-BEGIN
-    CREATE TABLE [$schema.metadata.encapsulation].[_$schema.metadata.positorSuffix] (
-        $schema.metadata.positorSuffix $schema.metadata.positorRange not null,
-        constraint pk_$schema.metadata.positorSuffix primary key (
-            $schema.metadata.positorSuffix asc
-        )
-    );
-    INSERT INTO [$schema.metadata.encapsulation].[_$schema.metadata.positorSuffix] (
-        $schema.metadata.positorSuffix
+CREATE TABLE IF NOT EXISTS \"$schema.metadata.encapsulation\".\"_$schema.metadata.positorSuffix\"(
+    \"$schema.metadata.positorSuffix\" $schema.metadata.positorRange not null,
+    constraint \"pk_$schema.metadata.positorSuffix\" primary key(
+        \"$schema.metadata.positorSuffix\"
     )
-    VALUES (
-        0 -- the default positor
-    );
-END
+);
+INSERT INTO \"$schema.metadata.encapsulation\".\"_$schema.metadata.positorSuffix\"(
+    \"$schema.metadata.positorSuffix\"
+)
+VALUES(
+    0 -- the default positor
+) ON CONFLICT DO NOTHING;
 GO
 ~*/
